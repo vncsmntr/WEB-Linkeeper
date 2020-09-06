08434855a9c14021a71b85ceea0cd286f2f53e7b
Vinicius Monteiro <vncsmntr@outlook.com>
2020-09-06 13:17:55 -0300

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
-    return <h1 > Link < /h1>
-
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/sign-in">Sign-In</Link>
+            </li>
+            <li>
+              <Link to="/sign-up">Sign-Up</Link>
+            </li>
+            <li>
+              <Link to="/manage/links/create">Create Link</Link>
+            </li>
+            <li>
+              <Link to="/manage/links/edit">Edit link</Link>
+            </li>
+            <li>
+              <Link to="/manage/links">Links</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/sign-in" />
+          <h1>SignIn</h1>
+          <Route path="/sign-up" />
+          <h1>SignUp</h1>
+          <Route path="/manage/links/create" />
+          <h1>Create link</h1>
+          <Route path="/manage/links/edit" />
+          <h1>Edit links</h1>
+          <Route path="/" />
+          <h1>Home</h1>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
